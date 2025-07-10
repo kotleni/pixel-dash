6da460214fb5253a5a50973de12048aca7d425ae
Viktor Varenik <yavarenikya@gmail.com>
2025-07-10 19:55:46 +0300

    refactor: reformat code with eslint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type {Metadata} from 'next';
-import {Geist, Geist_Mono} from 'next/font/google';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -14,9 +13,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body>
-                {children}
-            </body>
+            <body>{children}</body>
         </html>
     );
 }
